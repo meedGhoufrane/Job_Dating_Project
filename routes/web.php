caea1db2e14cfe869475eb09e857da86a4b09ed2
meed ghoufrane <baloncesmeed@gmail.com>
2024-01-31 11:03:45 +0100

    edit

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/hello', function () {
+//     return view('hello');
+// });
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
